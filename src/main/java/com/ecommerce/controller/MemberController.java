b648b16caf6fe5f30ceec19484249c59f5f8d7c2
hirahul0011 <ra_10001@yahoo.co.in>
2023-10-20 02:20:27 +0530

    edit profile action controller and dashboard jsp finalized

--- src/main/java/com/ecommerce/controller/MemberController.java
+++ src/main/java/com/ecommerce/controller/MemberController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.ecommerce.entity.User;
 import com.ecommerce.service.UserService;
@@ -29,6 +30,62 @@ public class MemberController {
 	  map.addAttribute("user", user);
 	
 	    return "edit-profile"; 
+	}
+
+	@RequestMapping(value = "/editprofileaction", method = RequestMethod.POST)
+	public String editProfileAction(ModelMap map,
+			HttpServletRequest request, 
+			@RequestParam(value="user_id", required=true) String userId,
+			 @RequestParam(value="pwd", required=true) String pwd,
+			 @RequestParam(value="pwd2", required=true) String pwd2,
+			 @RequestParam(value="fname", required=true) String fname,
+			 @RequestParam(value="lname", required=true) String lname,
+			 @RequestParam(value="age", required=true) String age,
+			 @RequestParam(value="address", required=true) String address) 
+	{
+	
+	  HttpSession session = request.getSession();
+	  if (session.getAttribute("user_id") == null) {
+		  return "login";
+	  }
+	
+	  User user = userService.getUserById((Long) session.getAttribute("user_id"));
+	  map.addAttribute("user", user);
+	
+	  if (pwd == null || pwd2 == null || pwd.equals("") || pwd2.equals("")) {
+		  map.addAttribute("error", "Error , Incomplete passwords submitted.");
+		  return "edit-profile";
+	  }
+	
+	  if (!pwd.equals(pwd2)) {
+		  map.addAttribute("error", "Error , Passwords do not match.");
+		  return "edit-profile";
+	  }
+	
+	  if (fname == null || fname.equals("")) {
+		  map.addAttribute("error", "First name is required.");
+		  return "edit-profile";
+	  }
+	
+	  if (lname == null || lname.equals("")) {
+		  map.addAttribute("error", "Last name is required.");
+		  return "edit-profile";
+	  }
+	  if (age == null || age.equals("")) {
+		  age = "0";
+	  }
+	
+	
+	
+	  user.setFname(fname);
+	  user.setLname(lname);
+	  user.setAge(Integer.parseInt(age));
+	  user.setAddress(address);
+	  user.setPwd(pwd);
+	
+	  userService.updateUser(user);
+	
+	    return "redirect:dashboard"; 
 	}
 
 }
