a87b144ccb0bfa84d70f97ce287ba78e5b8c8bab
hirahul0011 <ra_10001@yahoo.co.in>
2023-10-20 03:21:37 +0530

    admin login and admin login action controller with related admin login and admin dashboard jsp finalized

---
+++ src/main/java/com/ecommerce/controller/AdminController.java
@@ -0,0 +1,60 @@
+package com.ecommerce.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.ecommerce.entity.Admin;
+import com.ecommerce.service.AdminService;
+import com.ecommerce.service.CategoryService;
+import com.ecommerce.service.ProductService;
+import com.ecommerce.service.PurchaseItemService;
+import com.ecommerce.service.PurchaseService;
+import com.ecommerce.service.UserService;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+
+@Controller
+public class AdminController {
+
+	@Autowired
+	private AdminService adminService;
+	@Autowired
+	private CategoryService categoryService;
+	@Autowired
+	private ProductService productService;
+	@Autowired
+	private PurchaseItemService purchaseItemService;
+	@Autowired
+	private PurchaseService purchaseService;
+	@Autowired
+	private UserService userService;
+	@RequestMapping(value = "/adminlogin", method = RequestMethod.GET)
+	public String login(ModelMap map, HttpServletRequest request) 
+	{
+	  map.addAttribute("pageTitle", "ADMIN LOGIN");
+	    return "admin/login"; 
+	}
+	@RequestMapping(value = "/adminloginaction", method = RequestMethod.POST)
+	public String loginAction(ModelMap map, HttpServletRequest request, 
+			 @RequestParam(value="admin_id", required=true) String adminId,
+			 @RequestParam(value="admin_pwd", required=true) String adminPwd) 
+	{
+	
+	  Admin admin = adminService.authenticate(adminId, adminPwd);
+	  if (admin == null) { 
+		  map.addAttribute("error", "Admin login failed");
+		  return "admin/login";
+	  }
+	  // store admin id in session
+	  HttpSession session = request.getSession();
+	  session.setAttribute("admin_id", admin.getID());
+	
+	    return "admin/dashboard"; 
+	}
+
+}
