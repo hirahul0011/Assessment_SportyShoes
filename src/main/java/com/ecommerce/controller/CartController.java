193ae03371745920b3a48cecfad39683fba3f827
hirahul0011 <ra_10001@yahoo.co.in>
2023-10-20 02:51:35 +0530

    cart add item controller finalized

--- src/main/java/com/ecommerce/controller/CartController.java
+++ src/main/java/com/ecommerce/controller/CartController.java
@@ -8,8 +8,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.ecommerce.entity.CartItem;
+import com.ecommerce.entity.Product;
 import com.ecommerce.service.ProductService;
 import com.ecommerce.service.PurchaseItemService;
 import com.ecommerce.service.PurchaseService;
@@ -83,5 +86,55 @@ public class CartController {
 		  }
 		  return retVal;
 	  }
+
+	@SuppressWarnings("unchecked")
+		@RequestMapping(value = "/cartadditem", method = RequestMethod.GET)
+		    public String cartAddItem(ModelMap map, HttpServletRequest request,
+		    		@RequestParam(value="id", required=true) String productId,
+		    		RedirectAttributes redir) 
+		    {
+			  
+			  String page="redirect:cart";
+			  
+			  // check if user is logged in
+			  HttpSession session = request.getSession();
+			  if (session.getAttribute("user_id") == null) {
+	//		  if (user_id == null) {
+				  redir.addFlashAttribute("error", "Error, You need to login before adding items to cart");
+	//			  map.addAttribute("error", "Error, You need to login before adding items to cart");
+	//			  new DispatcherServlet();
+	//			  new HomeController().home(map, request);
+				  page="redirect:/";
+			  } else {
+				  
+				  long idValue = Long.parseLong(productId);
+				  // if cart is already in session then retrieve it else create a new cart list and 
+				  // save it to session
+				  List<CartItem> cartItems = new ArrayList<CartItem>();
+				  if (session.getAttribute("cart_items") != null)
+	//			  if (cart_items != null)
+					  cartItems = (List<CartItem>) session.getAttribute("cart_items");
+	//				  cartItems = (List<CartItem>) cart_items;
+				  if (isItemInCart(cartItems, idValue)) {
+					  redir.addFlashAttribute("error", "This item is already in your cart");
+					  page="redirect:/";
+				  } else {
+					  Product product = productService.getProductById(idValue);
+					  CartItem item = new CartItem();
+					  item.setProductId(idValue);
+					  item.setQty(1);
+					  item.setRate(product.getPrice());
+					  BigDecimal dprice = item.getRate().multiply(new BigDecimal(item.getQty())); 
+					  item.setPrice(dprice); 
+					  item.setName(product.getName()); 
+					  cartItems.add(item);
+					  
+					  session.setAttribute("cart_items", cartItems);
+	//				  map.addAttribute("cart_items", cartItems);
+				  }
+			  }
+			  
+		        return page; 
+		    }
 
 }
