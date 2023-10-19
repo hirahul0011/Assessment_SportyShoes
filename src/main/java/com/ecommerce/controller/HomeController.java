710d404e4f84d79b7c9d487dd50fa02db89399ed
hirahul0011 <ra_10001@yahoo.co.in>
2023-10-20 01:59:02 +0530

    home controller and related index.jsp finalized along with header footer and topbar jsp files

---
+++ src/main/java/com/ecommerce/controller/HomeController.java
@@ -0,0 +1,47 @@
+package com.ecommerce.controller;
+
+import java.util.HashMap;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.ecommerce.entity.Category;
+import com.ecommerce.entity.Product;
+import com.ecommerce.service.CategoryService;
+import com.ecommerce.service.ProductService;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+
+@Controller
+public class HomeController {
+
+	@Autowired
+	private CategoryService categoryService;
+	@Autowired
+	private ProductService productService;
+	@RequestMapping(value = {"/", "/home"}, method = RequestMethod.GET)
+	public String home(ModelMap map,  HttpServletRequest request) 
+	{
+	    HttpSession session = request.getSession();
+		List<Product> list = productService.getAllProducts();
+		
+		// use MAP to map the category names to product rows
+		 HashMap<Long, String> mapCats = new HashMap<Long, String>();
+		  for(Product product: list) {
+			  Category category = categoryService.getCategoryById(product.getCategoryId());
+			  if (category != null)
+				  mapCats.put(product.getID(), category.getName());
+		  }
+		  
+		map.addAttribute("list", list);
+		map.addAttribute("mapCats", mapCats);
+	    map.addAttribute("pageTitle", "SPORTY SHOES - HOMEPAGE");		    
+	    return "index"; 
+	}
+
+}
